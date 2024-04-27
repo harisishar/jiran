09c56d33519a2d95cf8098f76fbafda605c9f45f
harisishar <abu@abus-MacBook-Air.local>
2024-04-27 12:36:44 +0800

    second add file

---
+++ src/navigation/vertical/index.ts
@@ -0,0 +1,46 @@
+// ** Type import
+import { VerticalNavItemsType } from 'src/@core/layouts/types'
+
+const navigation = (): VerticalNavItemsType => {
+  return [
+    {
+      title: 'Home',
+      path: '/home',
+      icon: 'mdi:home-outline'
+    },
+    {
+      title: 'Billing',
+      path: '/billing',
+      icon: 'mdi:email-outline'
+    },
+    {
+      title: 'Visitor',
+      path: '/visitor',
+      icon: 'pepicons-pencil:people'
+    },
+    {
+      title: 'Complaints',
+      path: '/complaints',
+      icon: 'mingcute:report-fill'
+    },
+    {
+      title: 'Annoucements',
+      path: '/annoucements',
+      icon: 'mdi:bullhorn'
+    },
+    {
+      title: 'Residents',
+      path: '/residents',
+      icon: 'fluent:people-queue-24-regular'
+    },
+    {
+      path: '/acl',
+      action: 'read',
+      subject: 'acl-page',
+      title: 'Access Control',
+      icon: 'mdi:shield-outline'
+    }
+  ]
+}
+
+export default navigation
